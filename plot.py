b1f84a6e9249a7970c68f548dddbdfeffb2a5d90
William Woodall <william@osrfoundation.org>
2013-04-03 23:07:20 -0700

    Added some filtering

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ from pylab import *
 
 from utm import latlong2utm
 
-gps_file = 'out-and-back/gps.csv'
+gps_file = 'grass/gps.csv'
 
 
 def get_gps():
@@ -23,6 +23,27 @@ def get_gps():
         seq.append(int(x[1]))
         lat.append(float(x[6]))
         lng.append(float(x[7]))
+    for i, x in enumerate(lat):
+        if i == 0:
+            continue
+        if abs(lat[i-1] - x) > 10:
+            print(i)
+            del lat[i]
+            del lng[i]
+            del seq[i]
+    for i, x in enumerate(lng):
+        if i == 0:
+            continue
+        if abs(lng[i-1] - x) > 10:
+            print(i)
+            del lat[i]
+            del lng[i]
+            del seq[i]
+    subplot(211)
+    plot(seq, lat)
+    subplot(212)
+    plot(seq, lng)
+    show()
     utmx = []
     utmy = []
     for lati, lngi in zip(lat, lng):
@@ -37,16 +58,27 @@ def main():
     utmx_filt = [x - utmx[0] for x in utmx]
     utmy_filt = [y - utmy[0] for y in utmy]
     for i, x in enumerate(utmx_filt):
+        if i == 0:
+            continue
         if abs(utmx_filt[i-1] - x) > 10:
-            print(i)
+            print('x', i)
             del utmx_filt[i]
             del utmy_filt[i]
             del seq[i]
-    subplot(311)
-    plot(seq, utmx_filt)
-    subplot(312)
-    plot(seq, utmy_filt)
-    subplot(313)
+    for i, x in enumerate(utmy_filt):
+        if i == 0:
+            continue
+        if abs(utmy_filt[i-1] - x) > 10:
+            print('y', i)
+            del utmx_filt[i]
+            del utmy_filt[i]
+            del seq[i]
+    # subplot(311)
+    # plot(seq, utmx_filt)
+    # subplot(312)
+    # plot(seq, utmy_filt)
+    # subplot(313)
+    print(len(seq))
     axis('equal')
     scatter(utmx_filt, utmy_filt, c=seq)
     show()
