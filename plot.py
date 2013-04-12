4e49775d130ddd920f66e1aed0574b236db0fbcf
trainman419 <legotown@aol.com>
2013-04-12 15:28:54 -0700

    Plotting odom and GPS on the same axes.

--- plot.py
+++ plot.py
@@ -41,11 +41,11 @@ def get_gps(data_dir):
             del lat[i]
             del lng[i]
             del time[i]
-    subplot(211)
-    plot(time, lat)
-    subplot(212)
-    plot(time, lng)
-    show()
+    #subplot(211)
+    #plot(time, lat)
+    #subplot(212)
+    #plot(time, lng)
+    #show()
     utmx = []
     utmy = []
     for lati, lngi in zip(lat, lng):
@@ -54,6 +54,32 @@ def get_gps(data_dir):
         utmy.append(y)
     return time, utmx, utmy
 
+def get_odom(data_dir):
+    odom_file = os.path.join(data_dir, 'odom.csv')
+
+    if not os.path.exists(odom_file):
+        sys.exit('no ' + odom_file)
+    with open(odom_file) as f:
+        raw = f.read()
+    for x in raw.splitlines()[:5]:
+        print(x)
+    time = []
+    pos_x = []
+    pos_y = []
+    orientation = []
+    for x in raw.splitlines()[1:]:
+        x = x.split(',')
+        time.append(float(x[0]))
+        pos_x.append(float(x[7]))
+        pos_y.append(float(x[8]))
+
+    #subplot(211)
+    #plot(time, pos_x)
+    #subplot(212)
+    #plot(time, pos_y)
+    #show()
+
+    return time, pos_x, pos_y
 
 def main():
     if len(sys.argv) != 2:
@@ -62,9 +88,23 @@ def main():
 
     data_dir = sys.argv[1]
 
-    time, utmx, utmy = get_gps(data_dir) or [None, None, None]
+    gps_time, utmx, utmy = get_gps(data_dir) or [None, None, None]
+    odom_time, odom_x, odom_y = get_odom(data_dir)
+
     utmx_filt = [x - utmx[0] for x in utmx]
     utmy_filt = [y - utmy[0] for y in utmy]
+
+    odom_x_filt = [x - odom_x[0] for x in odom_x]
+    odom_y_filt = [y - odom_y[0] for y in odom_y]
+
+    min_time = min(odom_time[0], gps_time[0])
+    max_time = max(odom_time[-1], gps_time[-1])
+    total_time = max_time - min_time
+
+    odom_color = [(t - min_time)/total_time for t in odom_time]
+    gps_color = [(t - min_time)/total_time for t in gps_time]
+    print odom_color
+
     for i, x in enumerate(utmx_filt):
         if i == 0:
             continue
@@ -72,7 +112,7 @@ def main():
             print('x', i)
             del utmx_filt[i]
             del utmy_filt[i]
-            del time[i]
+            del gps_time[i]
     for i, x in enumerate(utmy_filt):
         if i == 0:
             continue
@@ -80,15 +120,17 @@ def main():
             print('y', i)
             del utmx_filt[i]
             del utmy_filt[i]
-            del time[i]
+            del gps_time[i]
     # subplot(311)
     # plot(time, utmx_filt)
     # subplot(312)
     # plot(time, utmy_filt)
     # subplot(313)
-    print(len(time))
+    print(len(gps_time))
     axis('equal')
-    scatter(utmx_filt, utmy_filt, c=time)
+    scatter(utmx_filt, utmy_filt, c=gps_color)
+    #plot(utmx_filt, utmy_filt, c=gps_color)
+    scatter(odom_x_filt, odom_y_filt, c=odom_color)
     show()
 
 
