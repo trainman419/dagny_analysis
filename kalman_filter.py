d99962b0008145e29f651aa93c2990ff0e75d260
trainman419 <legotown@aol.com>
2013-04-16 00:55:55 -0700

    Fix steering interpolation and tune mag offsets.

--- kalman_filter.py
+++ kalman_filter.py
@@ -1,4 +1,4 @@
-from pylab import plot, scatter, show, axis, quiver
+from pylab import plot, scatter, show, axis, quiver, savefig, cla
 
 from plot import get_series, get_gps_utm
 
@@ -33,9 +33,9 @@ def steer_to_radius(steer):
         # Extrapolate
         delta = steer - index
         if index != 100:
-            step = steer_to_radius_map[index + 10] - radius
+            step = (steer_to_radius_map[index + 10] - radius) / 10.0
         else:
-            step = steer_to_radius_map[100] - steer_to_radius_map[90]
+            step = (steer_to_radius_map[100] - steer_to_radius_map[90]) / 10.0
         radius += step * delta
     return radius
 
@@ -79,19 +79,32 @@ def kf_filter(enc_time, enc_count, enc_steer):
     pass
 
 
-def dead_recon(enc_time, enc_count, enc_steer, gps_time, utm_x, utm_y, odomx, odomy, magx, magy):
+def dead_reckon(enc_time, enc_count, enc_steer, gps_time, utm_x, utm_y, odomx, odomy, magx, magy, **kwargs):
     xs = [0]
     ys = [0]
     thetas = [0]
-    ox = 0.036923
-    oy = 0.107692
-    os = 1.5
-    magx = [x - ox*os for i, x in enumerate(magx) if i % 2 == 0]
-    magy = [y - oy*os for i, y in enumerate(magy) if i % 2 == 0]
+
+    ox = -0.025
+    if 'ox' in kwargs:
+        ox = kwargs['ox']
+    oy = 0.150
+    if 'oy' in kwargs:
+        oy = kwargs['oy']
+
+    steer_offset = 8
+    if 'steer_offset' in kwargs:
+        steer_offset = kwargs['steer_offset']
+
+    compass_offset = -0.6
+    if 'compass_offset' in kwargs:
+        compass_offset = kwargs['compass_offset']
+
+    magx = [x - ox for i, x in enumerate(magx) if i % 2 == 0]
+    magy = [y - oy for i, y in enumerate(magy) if i % 2 == 0]
     for dtime, dcount, steer, magxn, magyn in zip(pairwise_diff(enc_time), enc_count, enc_steer, magx, magy):
-        dist = dcount * Q_SCALE * 1.3
+        dist = dcount * Q_SCALE
         yaw = thetas[-1]
-        steer += 2
+        steer += steer_offset
         radius = steer_to_radius(steer)
         if steer == 0:
             dx = dist * cos(yaw)
@@ -111,7 +124,8 @@ def dead_recon(enc_time, enc_count, enc_steer, gps_time, utm_x, utm_y, odomx, od
 
         xs.append(xs[-1] + dx)
         ys.append(ys[-1] + dy)
-        thetas.append(atan2(magxn, magyn) + 0.1)
+        #thetas.append(yaw + dtheta)
+        thetas.append(atan2(magxn, magyn) + compass_offset)
     #for
 
     axis('equal')
@@ -120,7 +134,12 @@ def dead_recon(enc_time, enc_count, enc_steer, gps_time, utm_x, utm_y, odomx, od
     plot([x * -1 for x in xs], [y * -1 for y in ys])
     plot(odomx, odomy)
     # quiver(xs, ys, [cos(x) for x in thetas], [sin(x) for x in thetas], minlength=2)
-    show()
+    if 'filename' in kwargs:
+        print kwargs['filename']
+        savefig(kwargs['filename'], dpi=300)
+        cla()
+    else:
+        show()
 
 
 def main():
@@ -156,7 +175,15 @@ def main():
     odom_x = delta_from_start(odom_x, reverse=True)
     odom_y = delta_from_start(odom_y, reverse=True)
 
-    dead_recon(enc_time, enc_count, enc_steer, gps_time, utmx, utmy, odom_x, odom_y, mag_x, mag_y)
+    dead_reckon(enc_time, enc_count, enc_steer, gps_time, utmx, utmy, odom_x, odom_y, mag_x, mag_y)
+    #for ox in [ -0.026, -0.025, -0.024 ]:
+    #    for oy in [ 0.149, 0.15, 0.151 ]:
+    #        dead_reckon(enc_time, enc_count, enc_steer, gps_time, utmx, utmy, odom_x, odom_y, mag_x, mag_y, ox=ox, oy=oy, filename="mag-%f-%f.png"%(ox,oy))
+            
+    #for i in [ 0, 2, 4, 5, 6, 7, 8, 9, 10 ]:
+    #    dead_reckon(enc_time, enc_count, enc_steer, gps_time, utmx, utmy, odom_x, odom_y, mag_x, mag_y, steer_offset=i, filename="steer-%d.png"%(i))
+    #for i in [ -1.0, -0.8, -0.6, -0.4, -0.2, -0.1, 0, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0 ]:
+    #    dead_reckon(enc_time, enc_count, enc_steer, gps_time, utmx, utmy, odom_x, odom_y, mag_x, mag_y, compass_offset=i, filename="compass-%f.png"%(i))
 
 if __name__ == '__main__':
     main()
